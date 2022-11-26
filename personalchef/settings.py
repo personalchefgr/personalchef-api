615e4731f7730b2ef18e724b86f41d9526fcfc00
akotsampaseris <a.kotsampaseris@gmail.com>
2022-11-26 22:50:49 +0000

    Deploy to heroku

--- personalchef/settings.py
+++ personalchef/settings.py
@@ -13,8 +13,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['localhost']
-CORS_ALLOWED_ORIGINS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS').split(',')
 
 # Application definition
 INSTALLED_APPS = [
