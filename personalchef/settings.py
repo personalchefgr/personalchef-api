77c18b2c594b5b6063f66b58af554ec3971fc603
akotsampaseris <a.kotsampaseris@gmail.com>
2022-11-28 12:10:11 +0000

    Updated settings ALLOWED_HOSTS & CORS_ALLOWED_ORIGINS

--- personalchef/settings.py
+++ personalchef/settings.py
@@ -13,8 +13,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['localhost']
-CORS_ALLOW_ALL_ORIGINS = True
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
+CORS_ALLOWED_ORIGINS = [os.environ.get('CORS_ALLOWED_ORIGINS')]
 
 # Application definition
 INSTALLED_APPS = [
